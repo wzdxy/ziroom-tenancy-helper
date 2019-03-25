305c1d50f50b00504aad76ae8fac6595190f8bae
zc <zc826440930@yeah.net>
2019-03-26 01:55:46 +0800

    更换目标地点

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // 工作地点坐标 可在 http://api.map.baidu.com/lbsapi/getpoint/index.html 获取
   origin: {
-    x: 40.000865,
-    y: 116.486121
+    x: 40.025434,
+    y: 116.47314
   },
   originString: '40.000865,116.486121',
   // 百度地图 api ak
   ak: '',
   // 所有要搜索的关键字
-  keywordsArray: ['望京', '酒仙桥', '大山子', '太阳宫', '来广营', '三元桥'],
+  keywordsArray: ['望京', '太阳宫', '来广营', '亚运村小营', '北苑'],
   // 搜索条件
   searchFilter: {
     // 城市代码 默认北京
     city_code: 110000,
-    // 房屋类型 1-友家
-    type: 1
+    // 房屋类型 1-友家  12-整租2居
+    type: '12'
   },
   // 搜索的平均频率 (ms)
   avgDuration: 5000,
