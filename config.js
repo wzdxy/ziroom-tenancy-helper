e1c010d784ae2549a47f951a4c640a7f94bb6fae
wzdxy <zc_93@qq.com>
2019-04-19 01:30:05 +0800

    优化配置

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 module.exports = {
   // 工作地点坐标 可在 http://api.map.baidu.com/lbsapi/getpoint/index.html 获取
-  origin: {
-    x: 40.025434,
-    y: 116.47314
-  },
-  originString: '40.000865,116.486121',
+  // 不过和这里的经纬度是相反的, 需自己调转一下 :)
+  originString: '40.026884,116.474067',
   // 百度地图 api ak
   ak: '',
   // 所有要搜索的关键字
-  keywordsArray: ['望京', '太阳宫', '来广营', '亚运村小营', '北苑'],
+  keywordsArray: ['善各庄','来广营','阜通','望京南','将台','东风北桥','枣营','金台路'],
   // 搜索条件
   searchFilter: {
     // 城市代码 默认北京
